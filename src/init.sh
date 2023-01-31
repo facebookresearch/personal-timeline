7cc8c4b0e0e07402e685cf53d1c98f0325bcfa7b
Tripti Singh <triptisingh@yahoo.com>
2023-01-30 22:04:00 -0800

    Move css to static folder

--- src/init.sh
+++ src/init.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 mkdir -p ~/personal-data/app_data/static
+cp src/frontend/static/* ~/personal-data/app_data/static/.
 ln -s ~/personal-data
 touch env/ingest.env.list
 touch env/frontend.env.list
