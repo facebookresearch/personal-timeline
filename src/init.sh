cce4d5c72a67a0533a2fe6a843edb75592e4c3e8
Yuliang Li <yuliangli@fb.com>
2023-06-11 17:12:27 -0700

    fix init

--- src/init.sh
+++ src/init.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 mkdir -p ~/personal-data/app_data/static
-cp src/frontend/static/* ~/personal-data/app_data/static/.
+# cp src/frontend/static/* ~/personal-data/app_data/static/.
 ln -s ~/personal-data
 mkdir env
 touch env/ingest.env.list
